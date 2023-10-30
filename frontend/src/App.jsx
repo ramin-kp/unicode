e47d7ed531c32130ff4a7c6f3ce3ed4e5273ece0
Ramin Karimpor <ramin.kp81@gmail.com>
2023-10-30 19:04:17 +0330

    Color and fonts configuration of the app

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,24 @@
-
 function App() {
-  return <div>App</div>;
+  return (
+    <div>
+      <h1 className="text-xl m-5 font-dana bg-black-100 p-6">رامین کریم پوربناب خوش آمدید</h1>
+      <h1 className="text-xl m-5 font-danaLight bg-black-200 p-6">
+        رامین کریم پوربناب خوش آمدید
+      </h1>
+      <h1 className="text-xl m-5 font-danaMedium bg-black-300 p-6">
+        رامین کریم پوربناب خوش آمدید
+      </h1>
+      <h1 className="text-xl m-5 font-danaBold bg-black-400 p-6">
+        رامین کریم پوربناب خوش آمدید
+      </h1>
+      <h1 className="text-xl m-5 font-danaDemiBold bg-black-500 p-6">
+        رامین کریم پوربناب خوش آمدید
+      </h1>
+      <h1 className="text-xl m-5 font-morabbaBold bg-primary text-secondary p-6">
+        رامین کریم پوربناب خوش آمدید
+      </h1>
+    </div>
+  );
 }
 
 export default App;
---
+++ frontend/tailwind.config.js
@@ -0,0 +1,30 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  darkMode: "class",
+  theme: {
+    extend: {
+      fontFamily: {
+        danaLight: "danaLight",
+        dana: "dana",
+        danaMedium: "danaMedium",
+        danaBold: "danaBold",
+        danaDemiBold: "danaDemiBold",
+        morabbaMedium: "morabbaMedium",
+        morabbaBold: "morabbaBold",
+      },
+      colors: {
+        black: {
+          100: "#4a4b6d",
+          200: "#3c3f58",
+          300: "#32334d",
+          400: "#28293d",
+          500: "#1c1c28",
+        },
+        primary: "#2ed573",
+        secondary: "#4e81fb",
+      },
+    },
+  },
+  plugins: [],
+};
