fe94b70afeb1147dfa92fb9238f7793f1ee193d7
Ramin Karimpor <ramin.kp81@gmail.com>
2023-11-06 22:46:32 +0330

    routes test

--- src/routes.jsx
+++ src/routes.jsx
@@ -1,5 +1,6 @@
 import NotFound from "./components/NotFound/NotFound";
 import Courses from "./pages/Courses";
+
 import Home from "./pages/Home";
 const routes = [
   { path: "/", element: <Home /> },
