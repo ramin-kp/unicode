be54051b3b1c87f690b8de2826e796ccd2e0a781
Ramin Karimpor <ramin.kp81@gmail.com>
2023-11-06 22:13:44 +0330

    add notFound components

--- src/routes.jsx
+++ src/routes.jsx
@@ -1,7 +1,9 @@
+import NotFound from "./components/NotFound/NotFound";
 import Courses from "./pages/Courses";
 import Home from "./pages/Home";
 const routes = [
   { path: "/", element: <Home /> },
   { path: "/courses", element: <Courses /> },
+  { path: "/*", element: <NotFound /> },
 ];
 export default routes;
