9b7b33e1a47c84633e22f8097f785395a4bab086
Ramin Karimpor <ramin.kp81@gmail.com>
2023-11-06 22:25:49 +0330

    routes test

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,21 @@
-import routes from "./routes";
-import { useRoutes } from "react-router-dom";
+import NotFound from "./components/NotFound/NotFound";
+import Courses from "./pages/Courses";
+import Home from "./pages/Home";
+// import routes from "./routes";
+import { Navigate, Route, Router, Routes} from "react-router-dom";
 
 function App() {
-  const router = useRoutes(routes);
-  return <div>{router}</div>;
+  // const router = useRoutes(routes);
+  return (
+    <>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/courses" element={<Courses />} />
+        <Route path="/notfound" element={<NotFound />} />
+        <Route path="/*" element={<Navigate to="/notfound" />} />
+      </Routes>
+    </>
+  );
 }
 
 export default App;
