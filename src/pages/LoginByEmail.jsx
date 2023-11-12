67ca10c88e6d0db9ece6f9b5e2a5a49a1bc5099a
Ramin Karimpor <ramin.kp81@gmail.com>
2023-11-12 10:56:42 +0330

    fix: fix the svg

--- src/pages/LoginByEmail.jsx
+++ src/pages/LoginByEmail.jsx
@@ -15,70 +15,74 @@ export default function LoginByEmail() {
     };
   }, [theme]);
   return (
-    <main class=" ">
-      <section class="flex justify-center items-center h-screen bg-gray-100 dark:bg-black-500 text-zinc-700">
-        <div class="w-100">
-          <div class="flex justify-center gap-4 mb-4">
+    <main className=" ">
+      <section className="flex justify-center items-center h-screen bg-gray-100 dark:bg-black-500 text-zinc-700">
+        <div className="w-100">
+          <div className="flex justify-center gap-4 mb-4">
             <img className="w-24 h-[58px]" src="/images/logo.png" alt="logo" />
             <div>
-              <h1 class="text-xl dark:text-white font-danaBold">یونی کٌد</h1>
-              <p class="tracking-widest dark:text-white">unicode.ir</p>
+              <h1 className="text-xl dark:text-white font-danaBold">
+                یونی کٌد
+              </h1>
+              <p className="tracking-widest dark:text-white">unicode.ir</p>
             </div>
           </div>
-          <div class=" flex flex-col justify-center items-center p-9 bg-white dark:bg-black-400 rounded-3xl shadow-md">
-            <h2 class="text-2xl dark:text-white font-danaBold">ورود</h2>
-            <p class="pt-4 pb-10 text-zinc-700 dark:text-secondary-600">
+          <div className=" flex flex-col justify-center items-center p-9 bg-white dark:bg-black-400 rounded-3xl shadow-md">
+            <h2 className="text-2xl dark:text-white font-danaBold">ورود</h2>
+            <p className="pt-4 pb-10 text-zinc-700 dark:text-secondary-600">
               حساب کاربری ندارید؟
               <Link
                 to="/signup"
-                class="text-green-500 hover:text-green-400 duration-300"
+                className="text-green-500 hover:text-green-400 duration-300"
               >
                 ثبت نام
               </Link>
             </p>
-            <div class="flex justify-between items-center bg-gray-100 dark:bg-black-300 w-full px-4 py-2 rounded-xl">
+            <div className="flex justify-between items-center bg-gray-100 dark:bg-black-300 w-full px-4 py-2 rounded-xl">
               <input
-                class="bg-transparent text-xl border-none outline-none dark:text-secondary-600 focus:ring-transparent"
+                className="bg-transparent text-xl border-none outline-none dark:text-secondary-600 focus:ring-transparent"
                 type="text"
                 placeholder="ایمیل"
               />
               <svg className="w-5 h-5 text-slate-500 dark:text-slate-400">
-                <use to="#email"></use>
+                <use href="#email"></use>
               </svg>
             </div>
-            <div class="flex justify-between items-center bg-gray-100 dark:bg-black-300 w-full rounded-xl px-4 py-2 mt-3">
+            <div className="flex justify-between items-center bg-gray-100 dark:bg-black-300 w-full rounded-xl px-4 py-2 mt-3">
               <input
-                class="bg-transparent text-xl border-none outline-none dark:text-secondary-600 focus:ring-transparent"
+                className="bg-transparent text-xl border-none outline-none dark:text-secondary-600 focus:ring-transparent"
                 type="password"
                 placeholder="رمز عبور"
               />
               <svg className="w-5 h-5 text-slate-500 dark:text-slate-400">
-                <use to="#lock"></use>
+                <use href="#lock"></use>
               </svg>
             </div>
             <Link
               to="#"
-              class="bg-green-500 text-white text-xl text-center w-full px-28 py-3 rounded-xl mt-5 duration-300 hover:bg-green-400 shadow-md"
+              className="bg-green-500 text-white text-xl text-center w-full px-28 py-3 rounded-xl mt-5 duration-300 hover:bg-green-400 shadow-md"
             >
               تایید
             </Link>
-            <div class="w-full border-t border-gray-100 dark:border-zinc-700 mt-4">
-              <div class="flex justify-between items-center mt-4">
-                <label for="#" class="flex items-center gap-2">
+            <div className="w-full border-t border-gray-100 dark:border-zinc-700 mt-4">
+              <div className="flex justify-between items-center mt-4">
+                <label htmlFor="#" className="flex items-center gap-2 cursor-pointer">
                   <input
-                    class="bg-slate-300 dark:bg-black-500 focus:ring-white border-none rounded-md"
+                    className="bg-slate-300 dark:bg-black-500 focus:ring-white border-none rounded-md"
                     type="checkbox"
+                    hidden
                   />
+                  <span className="w-4 h-4  bg-gray-100 dark:bg-black-500 rounded-sm"></span>
                   <Link
                     to="#"
-                    class="text-slate-500 dark:text-slate-400 text-sm hover:text-primary duration-300"
+                    className="text-slate-500 dark:text-slate-400 text-sm hover:text-primary duration-300"
                   >
                     مرا فراموش مکن
                   </Link>
                 </label>
                 <Link
                   to="#"
-                  class="text-slate-500 dark:text-slate-400 text-sm hover:text-primary duration-300"
+                  className="text-slate-500 dark:text-slate-400 text-sm hover:text-primary duration-300"
                 >
                   حریم خصوصی
                 </Link>
--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -15,74 +15,76 @@ export default function Register() {
     };
   }, [theme]);
   return (
-    <main class="h-screen bg-gray-100 dark:bg-black-500 text-zinc-700">
-      <section class="flex justify-center items-center h-full">
-        <div class="w-[400px]">
-          <div class="flex justify-center gap-4 mb-4">
+    <main className="h-screen bg-gray-100 dark:bg-black-500 text-zinc-700">
+      <section className="flex justify-center items-center h-full">
+        <div className="w-[400px]">
+          <div className="flex justify-center gap-4 mb-4">
             <img className="w-24 h-58" src="/images/logo.png" alt="logo" />
             <div>
-              <h1 class="text-xl dark:text-white font-danaBold">یونی کٌد</h1>
-              <p class="dark:text-white tracking-widest">unicode.ir</p>
+              <h1 className="text-xl dark:text-white font-danaBold">
+                یونی کٌد
+              </h1>
+              <p className="dark:text-white tracking-widest">unicode.ir</p>
             </div>
           </div>
-          <div class="flex flex-col justify-center items-center max-h-115 bg-white dark:bg-black-400 rounded-3xl px-9 py-8 shadow-md">
-            <h2 class="text-2xl dark:text-white font-danaBold">عضویت</h2>
-            <p class="text-zinc-700 dark:text-secondary-600 pt-4 pb-10">
-              قبلا ثبت نام کرده اید؟
-              <Link to="/login" class="text-green-500 hover:text-green-400">
-                وارد شوید
+          <div className="flex flex-col justify-center items-center max-h-115 bg-white dark:bg-black-400 rounded-3xl px-9 py-8 shadow-md">
+            <h2 className="text-2xl dark:text-white font-danaBold">عضویت</h2>
+            <p className="text-zinc-700 dark:text-secondary-600 pt-4 pb-10">
+               قبلا ثبت نام کرده اید؟ 
+              <Link to="/login" className="text-green-500 hover:text-green-400">
+                 وارد شوید 
               </Link>
             </p>
-            <div class="flex justify-between items-center w-full bg-gray-100 dark:bg-black-300 rounded-xl px-4 py-3">
+            <div className="flex justify-between items-center gap-x-3 w-full bg-gray-100 dark:bg-black-300 rounded-xl px-4 py-3">
               <input
-                class="bg-transparent text-lg dark:text-secondary-600 border-none outline-none"
+                className="grow px-1 bg-transparent text-lg dark:text-secondary-600 border-none outline-none"
                 type="text"
                 placeholder="نام کاربری"
               />
-              <svg class="w-5 h-5 text-slate-500 dark:text-slate-400">
+              <svg className="w-5 h-5 text-slate-500 dark:text-slate-400">
                 <use href="#user-outline"></use>
               </svg>
             </div>
-            <div class="flex justify-between items-center w-full bg-gray-100 dark:bg-black-300 rounded-xl px-4 py-3 mt-3">
+            <div className="flex justify-between items-center gap-x-3 w-full bg-gray-100 dark:bg-black-300 rounded-xl px-4 py-3 mt-3">
               <input
-                class="bg-transparent text-lg dark:text-secondary-600 border-none outline-none"
+                className="grow px-1 bg-transparent text-lg dark:text-secondary-600 border-none outline-none"
                 type="text"
                 placeholder="شماره موبایل"
               />
-              <svg class="w-5 h-5 text-slate-500 dark:text-slate-400">
+              <svg className="w-5 h-5 text-slate-500 dark:text-slate-400">
                 <use href="#phone"></use>
               </svg>
             </div>
-            <div class="flex justify-between items-center w-full bg-gray-100 dark:bg-black-300 rounded-xl px-4 py-3 mt-3">
+            <div className="flex justify-between items-center gap-x-3 w-full bg-gray-100 dark:bg-black-300 rounded-xl px-4 py-3 mt-3">
               <input
-                class="bg-transparent text-lg dark:text-secondary-600 border-none outline-none"
+                className="grow px-1 bg-transparent text-lg dark:text-secondary-600 border-none outline-none"
                 type="text"
                 placeholder="ایمیل"
               />
-              <svg class="w-5 h-5 text-slate-500 dark:text-slate-400">
+              <svg className="w-5 h-5 text-slate-500 dark:text-slate-400">
                 <use href="#email"></use>
               </svg>
             </div>
-            <div class="flex justify-between items-center w-full bg-gray-100 dark:bg-black-300 rounded-xl px-4 py-3 mt-3">
+            <div className="flex justify-between items-center gap-x-3 w-full bg-gray-100 dark:bg-black-300 rounded-xl px-4 py-3 mt-3">
               <input
-                class="bg-transparent text-lg dark:text-secondary-600 border-none outline-none"
+                className="grow px-1 bg-transparent text-lg dark:text-secondary-600 border-none outline-none"
                 type="password"
                 placeholder="رمز عبور"
               />
-              <svg class="w-5 h-5 text-slate-500 dark:text-secondary-600">
+              <svg className="w-5 h-5 text-slate-500 dark:text-secondary-600">
                 <use href="#lock"></use>
               </svg>
             </div>
             <Link
               to="#"
-              class="w-full bg-green-500 text-white text-xl text-center rounded-xl px-28 py-3 mt-5 duration-300 hover:bg-green-400 shadow-md"
+              className="w-full bg-green-500 text-white text-xl text-center rounded-xl px-28 py-3 mt-5 duration-300 hover:bg-green-400 shadow-md"
             >
               ادامه
             </Link>
           </div>
-          <p class="text-lg dark:text-secondary-600 mt-6">
+          <p className="text-lg dark:text-secondary-600 mt-6">
             با عضویت در سایت، تمامی{" "}
-            <span class="text-primary mt-4">شرایط و قوانین</span> استفاده‌از
+            <span className="text-primary mt-4">شرایط و قوانین</span> استفاده‌از
             خدمت یونی کٌد را پذیرفته اید.
           </p>
         </div>
